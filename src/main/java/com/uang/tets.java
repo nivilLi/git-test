d0c5aa1f205ccc7d1767575e9896411506c81e0f
lc <3220614623@qq.com>
2022-09-26 21:19:15 +0800

    vice third commit

--- src/main/java/com/uang/tets.java
+++ src/main/java/com/uang/tets.java
@@ -2,7 +2,7 @@ package com.uang;
 
 public class tets {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("daffaffyhjjjgdfg");
         System.out.println("hellofasfsa");
         System.out.println("hellofasfsa");
     }
