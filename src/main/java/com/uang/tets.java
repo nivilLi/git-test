20241edeb381bea763e732747a7dfe0fb2ae715c
lc <3220614623@qq.com>
2022-09-26 21:05:53 +0800

    vice first commit

--- src/main/java/com/uang/tets.java
+++ src/main/java/com/uang/tets.java
@@ -2,6 +2,8 @@ package com.uang;
 
 public class tets {
     public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("hellofasfsa");
         System.out.println("hellofasfsa");
     }
 }
