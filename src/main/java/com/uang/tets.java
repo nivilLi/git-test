b23032ca389950b762601b290b760182cfeadf14
lc <3220614623@qq.com>
2022-09-26 21:17:54 +0800

    vice second commit

--- src/main/java/com/uang/tets.java
+++ src/main/java/com/uang/tets.java
@@ -2,7 +2,10 @@ package com.uang;
 
 public class tets {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("gffasfsfsa");
+        System.out.println("hellofasfsa");
+        System.out.println("hellofasfsa");
+        System.out.println("hellofasfsa");
         System.out.println("hellofasfsa");
         System.out.println("hellofasfsa");
     }
